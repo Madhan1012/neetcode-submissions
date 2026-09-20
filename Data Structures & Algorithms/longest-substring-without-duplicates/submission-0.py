05b5884023058df6dd5ec520b8fa3e22647f046f
Madhan <150823749+Madhan1012@users.noreply.github.com>
2026-09-20 19:59:13 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = {}
+        longest = 0
+        left = 0
+        for right in range(len(s)):
+            if s[right] in seen and seen[s[right]] >= left:
+                left = seen[s[right]] + 1
+            seen[s[right]] = right
+            longest = max(longest, right - left + 1) 
+        return longest
